4ab76bfefe386d3c765a1b71ecd4fce0beaceb13
abeprincec <abeprincec@gmail.com>
2018-06-22 16:02:03 +0000

    finished admin html auth

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "jungle", password: "book"
+
   def index
     @categories = Category.all()
     puts @categories
